cab5aa22563bd4a05e4ddf41d0c5f8f23f709368
Betsy Haibel <betsy.haibel@gmail.com>
2018-06-08 16:19:11 -0400

    scaffold out Rails engine

---
+++ lib/ticketing.rb
@@ -0,0 +1,5 @@
+require "ticketing/engine"
+
+module Ticketing
+  # Your code goes here...
+end
---
+++ lib/ticketing/engine.rb
@@ -0,0 +1,5 @@
+module Ticketing
+  class Engine < ::Rails::Engine
+    isolate_namespace Ticketing
+  end
+end
