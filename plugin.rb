a0d44e614b66902d62e5400973526ec84b916ade
Betsy Haibel <betsy.haibel@gmail.com>
2018-06-09 14:12:25 -0400

    stub out sample JSON for a GET to the ticket index

--- plugin.rb
+++ plugin.rb
@@ -4,8 +4,12 @@
 # authors:
 # url: https://github.com/angusmcleod/discourse-ticketing
 
+require_relative './lib/ticketing'
+
 add_admin_route 'discourse_ticketing.nav_button_title', 'ticketing'
 
 Discourse::Application.routes.append do
   get '/admin/ticketing' => 'admin/plugins#index', constraints: StaffConstraint.new
+
+  mount Ticketing::Engine, at: '/ticketing', constraints: StaffConstraint.new
 end
