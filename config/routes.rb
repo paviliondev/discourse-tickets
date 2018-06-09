a635011abc067facdbfef6f872b661bdb3104073
Betsy Haibel <betsy.haibel@gmail.com>
2018-06-09 14:52:15 -0400

    add show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Ticketing::Engine.routes.draw do
-  resources :tickets do
-  end
   resource :tag_group_configuration
+  resources :tickets, only: [:index, :show], constraints: { id: /(topic|post)-\d+/ }
 end
