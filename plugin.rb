78e59aa726555853caa8ba24139ab7c36893c66d
Nathan Kleekamp <nkleekamp@gmail.com>
2018-06-09 11:47:02 -0400

    make the key less generic after feedback

--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 # authors:
 # url: https://github.com/angusmcleod/discourse-ticketing
 
-add_admin_route 'discourse_ticketing.title', 'ticketing'
+add_admin_route 'discourse_ticketing.nav_button_title', 'ticketing'
 
 Discourse::Application.routes.append do
   get '/admin/ticketing' => 'admin/plugins#index', constraints: StaffConstraint.new
