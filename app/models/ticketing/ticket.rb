a4ada96f1f123f6bc72849bf5a752aa59b05b9c1
Howard M. Miller <hmiller@customink.com>
2018-06-10 12:11:22 -0400

    add real assignee & assigner data to ticket api endpoint
    Co-authored-by: Robb Kidd <robb@thekidds.org>

--- app/models/ticketing/ticket.rb
+++ app/models/ticketing/ticket.rb
@@ -32,7 +32,9 @@ module Ticketing
         title: title,
         priority: priority,
         status: status,
-        dateDue: date_due, 
+        dateDue: date_due,
+        assignee: assignee,
+        creator: creator,
         href: Engine.routes.url_helpers.ticket_url(self, host: 'example.com')
       }.reverse_merge(a_ticket_blob)
     end
@@ -53,7 +55,23 @@ module Ticketing
       created_at + TagGroupConfiguration.sla_for_priority(priority)
     end
 
-    private def a_ticket_blob
+    private
+
+    def assignee
+      # assigned_to_user is defined in the discourse-assign plugin
+      # At some point, would we want to have a check here if the plugin isn't available?
+      result = BasicUserSerializer.new(_topic_or_post.assigned_to_user).as_json[:basic_user]
+      result.merge(username_lower: result[:username].downcase) if result
+    end
+
+    def creator
+      user = User.find_by(id: _topic_or_post.custom_fields["assigned_by_id"])
+      return unless user
+      result = BasicUserSerializer.new(user).as_json[:basic_user]
+      result.merge(username_lower: result[:username].downcase) if result
+    end
+
+    def a_ticket_blob
       {
         "id": 3424,
         "topicOrPost": 'post',
@@ -62,58 +80,6 @@ module Ticketing
         "status": 'waiting',
         "dateCreated": "2018-06-09T15:59:55.725Z",
         "dateDue": "2018-06-09T15:59:55.725Z",
-        "assignee": {
-          "id":3,
-          "username":"howardm",
-          "avatar_template":"/letter_avatar_proxy/v2/letter/h/f19dbf/{size}.png",
-          "active":false,
-          "admin":false,
-          "moderator":false,
-          "last_seen_at":nil,
-          "last_emailed_at": "2018-06-09T15:59:55.725Z",
-          "created_at": "2018-06-09T15:59:47.937Z",
-          "last_seen_age":nil,
-          "last_emailed_age":7156.707830402,
-          "created_at_age":7164.495096115,
-          "username_lower":"howardm",
-          "trust_level":1,
-          "manual_locked_trust_level":nil,
-          "flag_level":0,
-          "title":nil,
-          "suspended":false,
-          "time_read":0,
-          "staged":false,
-          "days_visited":0,
-          "posts_read_count":0,
-          "topics_entered":0,
-          "post_count":0
-        },
-        "creator": {
-          "id":3,
-          "username":"howardm",
-          "avatar_template":"/letter_avatar_proxy/v2/letter/h/f19dbf/{size}.png",
-          "active":false,
-          "admin":false,
-          "moderator":false,
-          "last_seen_at":nil,
-          "last_emailed_at": "2018-06-09T15:59:55.725Z",
-          "created_at": "2018-06-09T15:59:47.937Z",
-          "last_seen_age":nil,
-          "last_emailed_age":7156.707830402,
-          "created_at_age":7164.495096115,
-          "username_lower":"howardm",
-          "trust_level":1,
-          "manual_locked_trust_level":nil,
-          "flag_level":0,
-          "title":nil,
-          "suspended":false,
-          "time_read":0,
-          "staged":false,
-          "days_visited":0,
-          "posts_read_count":0,
-          "topics_entered":0,
-          "post_count":0
-        },
         "involvedUsers": [
           {
             "id":3,
